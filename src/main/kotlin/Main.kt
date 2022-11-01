514674086e1ccbd3a504445a55a3983e79721a2d
Christian Bouwense <bouwensec@gmail.com>
2022-11-01 12:44:08 -0400

    cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,3 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
-
-fun addInts(x: Int, y: Int): Int {
-    return x + y
-}
