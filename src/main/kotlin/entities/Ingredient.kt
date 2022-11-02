f1b5791c0464c05bbbcedbce35e780a1b143339c
Christian Bouwense <bouwensec@gmail.com>
2022-11-02 14:52:17 -0400

    implemented Ingredient data class

--- src/main/kotlin/entities/Ingredient.kt
+++ src/main/kotlin/entities/Ingredient.kt
@@ -2,7 +2,7 @@ package entities
 
 data class Ingredient(
     val name: String = "",
-    val priceInCents: Int = 0,
-    val measurementAmount: Float = 0.0f,
-    val measurementUnit: MeasurementUnit = MeasurementUnit.GRAMS,
+    var priceInCents: Int = 0,
+    var measurementAmount: Float = 0.0f,
+    var measurementUnit: MeasurementUnit = MeasurementUnit.GRAMS,
 )
